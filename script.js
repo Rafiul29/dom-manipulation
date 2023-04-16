0dc9cb069571d3f6255bd46045734ba27e2101d6
Rafiul29 <rafiulislam4472@gmail.com>
2023-04-17 00:56:08 +0600

    navbar effect

--- script.js
+++ script.js
@@ -1,25 +1,41 @@
-//select dom element
-
-const fullImg=document.querySelector('.full-img')
-const smallImg=document.querySelectorAll('.gallery img')
-const modal=document.querySelector('.modal')
-
-smallImg.forEach(function(img){
-img.addEventListener("click",function(){
-  modal.classList.add('open')
-  fullImg.classList.add("open")
-
-  //chnages the dynamic url
-const originalQuality=img.getAttribute("alt")
-fullImg.src=`img/full/${originalQuality}.jpg`
-})
 
 
-})
+const sections=document.querySelectorAll('section')
+const trans=document.querySelector(".trans")
+const grandiants=["coral","green","chocolate","tomato"]
 
-modal.addEventListener("click",function(e){
-  if(e.target.classList.contains("modal")){
-    modal.classList.remove("open")
-    fullImg.classList.remove("open")
-  }
+const option={
+  threshold:0.7
+}
+
+
+
+const observer=new IntersectionObserver(navScroll,option)
+
+function navScroll(entries){
+  entries.forEach(function(entry){
+
+    const className=entry.target.className;
+
+    const  activeLink=document.querySelector(`[data-page="${className}"]`)
+    const elementIndex=entry.target.getAttribute('data-index')
+    const coordinates=activeLink.getBoundingClientRect();
+      const directions={
+        height:coordinates.height,
+        width:coordinates.width,
+        top:coordinates.top,
+        left:coordinates.left,
+      }
+      if(entry.isIntersecting){
+        trans.style.setProperty('height',`${directions.height}px`)
+        trans.style.setProperty('width',`${directions.width}px`)
+        trans.style.setProperty('top',`${directions.top}px`)
+        trans.style.setProperty('left',`${directions.left}px`)
+        trans.style.backgroundColor=grandiants[elementIndex]
+      }
+  })
+}
+
+sections.forEach(function(section){
+  observer.observe(section)
 })
