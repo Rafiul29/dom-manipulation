a0b2e7ce455e62eb42c067c3f77804d567a7783b
Rafiul29 <rafiulislam4472@gmail.com>
2023-04-19 11:15:07 +0600

    custom animated progess bar

--- script.js
+++ script.js
@@ -1,54 +1,7 @@
+const progressEl=document.querySelector('.progress-done');
+setTimeout(function(){
+  progressEl.style.width=progressEl.getAttribute('data-done')+"%"
+ 
+  progressEl.style.opacity=1
+},1000)
 
-//selected dom element
-const sections=document.querySelectorAll('section')
-const trans=document.querySelector(".trans")
-const navlinks=document.querySelector('.nav-links')
-
-const grandiants=["coral","green","chocolate","tomato"]
-
-const option={
-  threshold:0.7
-}
-
-
-
-const observer=new IntersectionObserver(navScroll,option)
-
-function navScroll(entries){
-  entries.forEach(function(entry){
-
-    const className=entry.target.className;
-
-    const  activeLink=document.querySelector(`[data-page="${className}"]`)
-    const elementIndex=entry.target.getAttribute('data-index')
-    const coordinates=activeLink.getBoundingClientRect();
-      const directions={
-        height:coordinates.height,
-        width:coordinates.width,
-        top:coordinates.top,
-        left:coordinates.left,
-      }
-      if(entry.isIntersecting){
-        trans.style.setProperty('height',`${directions.height}px`)
-        trans.style.setProperty('width',`${directions.width}px`)
-        trans.style.setProperty('top',`${directions.top}px`)
-        trans.style.setProperty('left',`${directions.left}px`)
-        trans.style.backgroundColor=grandiants[elementIndex]
-      }
-  })
-}
-
-sections.forEach(function(section){
-  observer.observe(section)
-})
-
-
-//smooth scroll
-navlinks.addEventListener('click',(e)=>{
-  e.preventDefault();
-  if(e.target.classList.contains('nav-link')){
-    const id= e.target.getAttribute('href')
-    document.querySelector(id).scrollIntoView({behavior:"smooth"})
-   
-  }
-})
